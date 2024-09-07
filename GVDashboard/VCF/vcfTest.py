0229254753df92a9d97194c46755f43ae6fd6287
Joshua Jandrell <joshuarjandrell@gmail.com>
2024-09-07 16:48:36 +0200

    vcfttest example

--- GVDashboard/VCF/vcfTest.py
+++ GVDashboard/VCF/vcfTest.py
@@ -3,28 +3,50 @@
 import allel as al # Used for VCF file manipulation
 import os
 
-TEST_FILE = os.path.realpath("./Data/afr-small.vcf") # Path to the vcf file used for testing
-TEST_FILE = os.path.realpath("./Data/afr-small.vcf") # Path to the vcf file used for testing
+TEST_FILE = os.path.realpath("./Data/med.vcf.gz") # Path to the vcf file used for testing
 
 def getData()->dict:
     return al.read_vcf(TEST_FILE)
 
 
 if __name__ == "__main__":
+    # Get data somehow....
     data = getData()
+    # and make data into dataframe
     df = al.vcf_to_dataframe(TEST_FILE)
-    print(data.keys())
-    bool_search = (df["QUAL"].between(30, 35, inclusive = 'both')) & (df["POS"]<15000)
+
+    # Construct a search query based on dataframe
+    bool_search = (df["QUAL"].between(30, 35, inclusive = 'both'))
+    # this earch can be chained (useful)
+    bool_search = bool_search &(df["POS"]<15000)
+
+    # then get a new dataframe by indexing useing the bool search
     new_df = df[bool_search]
+
+    # Dataframe can also be sorted like this 
     new_df = new_df.sort_values(by=["QUAL"], ascending=True)
+
     print(new_df)
-    print(new_df.index)
 
     
-    # Use the dataframe index to access rows (or columns) for the genotype array
+    # The idexes of a dataframe can be used to access and sored rows of a genotype array (or any numpy array/matrix)
     print(al.GenotypeArray(data['calldata/GT'])[new_df.index,:])
 
-    # print(len(max(data['variants/REF'], key=len)))
-    # print((max(data['variants/REF'], key=len)))
-    # print(len(data['variants/REF']))
+    # get column of dtaframe
+    print(new_df["POS"])
+
     print(data['samples'])
+
+    eg_sample = data['samples'][0]
+
+    #============ String stuff =================
+    # Get only the numbers form a string
+    numb_str = ''.join([s for s in eg_sample if s.isdigit()])
+    print(numb_str)
+
+    # Now remove numb_str from the main sring 
+    leters = eg_sample.strip(numb_str)
+    print(leters)
+
+    # now make sample an intager
+    int(numb_str)
